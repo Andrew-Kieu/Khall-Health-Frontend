b53b4ea1795a3dfca7d837b2c9e787f22d7591a5
Andrew Kieu <andrew.kieu@gmail.com>
2024-02-27 22:43:15 -0600

    added the contact, jobs, signIn, and signOut components. also, added routing in header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent}, 
-    {path: 'about', component: AboutComponent}
-
+    {path: 'about', component: AboutComponent},
+    {path: 'jobs', component: JobsComponent},
+    {path: 'contact', component: ContactComponent}, 
+    {path: 'signIn', component: SignInComponent},
+    {path: 'signOut', component: SignOutComponent}
+    
 ];
