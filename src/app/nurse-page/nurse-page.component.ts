69e6df692cf8586ea79e8bff6e864ad0e363d97b
Andrew Kieu <andrew.kieu@gmail.com>
2024-03-09 17:21:59 -0600

    put the files into the corresponding components

---
+++ src/app/nurse-page/nurse-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nurse-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './nurse-page.component.html',
+  styleUrl: './nurse-page.component.css'
+})
+export class NursePageComponent {
+
+}
