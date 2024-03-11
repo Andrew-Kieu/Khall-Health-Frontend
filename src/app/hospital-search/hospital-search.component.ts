ed50ee2794140a3524e71330956c99b478ad840f
C00457312 <C00457312@louisiana.edu>
2024-03-10 21:43:35 -0500

    Incorporate Hospital Search into Angular

---
+++ src/app/hospital-search/hospital-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital-search',
+  templateUrl: './hospital-search.component.html',
+  styleUrls: ['./hospital-search.component.css']
+})
+export class HospitalSearchComponent {
+    
+}
