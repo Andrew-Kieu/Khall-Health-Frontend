af6173e17ed7d7ab1bb912be291f66f54f8eb355
C00457312 <C00457312@louisiana.edu>
2024-03-12 17:49:58 -0500

    Incorporated Hospital Search into Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignOutComponent } from './sign-out/sign-out.component';
 import { NursePageComponent } from './nurse-page/nurse-page.component';
+import { HospitalSearchComponent } from './hospital-search/hospital-search.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent}, 
@@ -15,4 +16,5 @@ export const routes: Routes = [
     {path: 'signIn', component: SignInComponent},
     {path: 'signOut', component: SignOutComponent},
     {path: 'nurse-page', component: NursePageComponent}
+    {path: 'hospital-page', component: HospitalSearchComponent}
 ];
