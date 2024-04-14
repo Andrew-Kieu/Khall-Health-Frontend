2bf9aebacdcb83f73d3d40e3f54dea145b31ec80
Kobi Lipari <kobiclipari@gmail.com>
2024-04-14 15:04:28 -0500

    Adding Tools Page

---
+++ src/app/tools-page/tools-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './tools-page.component.html',
+  styleUrl: './tools-page.component.css'
+})
+export class ToolsPageComponent {
+}
